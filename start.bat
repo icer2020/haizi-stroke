33692c6c7892b2cae3566e491f89da81ccd711d2
icer2020 <foxoon_lee@163.com>
2026-07-13 14:34:08 +0800

    feat: 添加 PWA 支持（manifest + Service Worker + 图标）

--- start.bat
+++ start.bat
@@ -6,9 +6,21 @@ REM @Author        :   ICer
 REM @Contact       :   i_chip_backend@163.com
 REM @WebSite       :   https://blog.csdn.net/i_chip_backend
 REM @License       :   (C)Copyright 2018-2026, ICerDev
-REM @Description   :   一键启动 HTTP 服务器
+REM @Description   :   一键启动 HTTP 服务器（含局域网地址）
 REM
 
 cd /d "%~dp0"
+echo.
+echo === 汉字笔画演示 ===
+echo.
+echo 手机在同 WiFi 下访问:
+for /f "skip=1 delims={}, " %%A in ('wmic nicconfig where "IPEnabled=True and not Description like '*Virtual*'" get IPAddress 2^>nul') do for %%B in (%%~A) do (
+  echo %%B | findstr /r "^[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*$" >nul && echo   http://%%B:8899/
+)
+echo.
+echo 本机访问: http://localhost:8899/
+echo.
+echo 首次访问后，Chrome 可"添加到主屏幕"离线使用
+echo.
 start http://localhost:8899/
 python -m http.server 8899
